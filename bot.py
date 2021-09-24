5d43f4eb21f840b2480c5e830c6847b7b8b9803d
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-24 20:25:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -172,36 +172,4 @@ async def telegraphphoto(client, message):
     finally:
         os.remove(download_location)
 
-@Peaky.on_message(filters.video)
-async def telegraphvid(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
-        message=message, file_name='root/jetg')
-    try:
-        response = upload_file(download_location)
-    except:
-        await msg.edit_text("Video size should be less than 5mb!") 
-    else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @JEBotZ**',
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
-
-@Peaky.on_message(filters.animation)
-async def telegraphgif(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
-        message=message, file_name='root/jetg')
-    try:
-        response = upload_file(download_location)
-    except:
-        await msg.edit_text("Gif size should be less than 5mb!") 
-    else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @JEBotZ**',
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
-
 Peaky.run()
