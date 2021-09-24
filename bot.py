5d41605612be2c1f0c69abbb032f6e7439756981
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-24 20:28:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,20 +156,4 @@ async def callback_data(bot, update: CallbackQuery):
     elif query_data == "close":
         await update.message.delete()
 
-@Peaky.on_message(filters.photo)
-async def telegraphphoto(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
-        message=message, file_name='root/jetg')
-    try:
-        response = upload_file(download_location)
-    except:
-        await msg.edit_text("Photo size should be less than 5mb!") 
-    else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @JEBotZ**',
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
-
 Peaky.run()
