508136ebef2ebb334b051de3c28fcece9b061264
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-24 12:26:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
+   
+    START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
+
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
