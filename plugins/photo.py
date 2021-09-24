a20858841720f3c741964c1e3b0a84c73f78c1b5
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-24 12:20:51 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -1,1 +1,14 @@
 
+@Client.on_message(filters.command('start') & filters.private)
+async def start(client, message):
+    await message.reply_photo(photo=Config.START_IMG, caption=Config.START_MSG.format(message.from_user.mention),
+         reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(BUTTON1, url=GITCLONE)
+                 ],[
+                    InlineKeyboardButton(OWNER, url=f"https://telegram.dog/{Config.OWNER}"),
+                    InlineKeyboardButton(ABS, url=B2)
+            ]
+          ]
+        ),
