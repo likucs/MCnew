f4586a404ec0e3a72bfd1f2c571b5e800ce29458
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-25 11:18:55 +0530

    Update call_back.py

--- plugins/call_back.py
+++ plugins/call_back.py
@@ -92,7 +92,7 @@ async def callback_data(bot, update: CallbackQuery):
     elif query_data == "home": 
         buttons = [[
             InlineKeyboardButton('🎧 𝚂𝙾𝙽𝙶', callback_data='song'),
-            InlineKeyboardButton('🔗 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='close'),
+            InlineKeyboardButton('🔗 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙿𝙷', callback_data='Telegraph'),
         ],[
             InlineKeyboardButton('📅 𝙸𝙽𝙵𝙾', callback_data='close'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 ', callback_data='close'),
