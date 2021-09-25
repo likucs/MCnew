b028d385394b7eb924eda250f37429e4c7aef450
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-25 09:19:37 +0530

    Update telegraphy.py

--- plugins/telegraphy.py
+++ plugins/telegraphy.py
@@ -1,1 +1,9 @@
-
+import os
+from telegraph import upload_file
+import pyrogram
+from pyrogram import filters, Client
+from sample_config import Config
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery)
