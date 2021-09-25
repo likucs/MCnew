86cf6a10442c56a1677b3192ea079da578311bc7
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-25 12:03:55 +0530

    Update sticker.py

--- plugins/sticker.py
+++ plugins/sticker.py
@@ -1,1 +1,11 @@
+from pyrogram import Client, filters
 
+# Sticker ID
+@Client.on_message(
+    filters.private
+    & ~filters.forwarded
+    & ~filters.command(["start", "about", "help", "id"])
+)
+async def stickers(idbot, msg):
+    if msg.sticker:
+        await msg.reply(f"This Sticker's ID is <code>{msg.sticker.file_id}</code>", quote=True)
