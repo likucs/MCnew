65c9f590f20b157168fbce9d17ffb46f6ba14935
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-25 18:29:21 +0530

    Update string.py

--- plugins/string.py
+++ plugins/string.py
@@ -27,7 +27,7 @@ PHONE_NUMBER_TEXT = (
 
 
 
-@Peaky.on_message(filters.private & filters.command("string"))
+@Client.on_message(filters.private & filters.command("string"))
 async def generate_str(c, m):
     get_api_id = await c.ask(
         chat_id=m.chat.id,
