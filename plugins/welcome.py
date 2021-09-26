e6f35fcc6e2e143c9cfec069bc10c8e87dc0d8a8
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-26 12:42:03 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -10,9 +10,9 @@ from pyrogram.types import InlineKeyboardMarkup
 
 @Client.on_message(filters.join_chat_members)
 async def welcome(bot,message):
-	chatid= message.chat.id
+	chatid=message.chat.id
 	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
-	,reply_markup = InlineKeyboardMarkup(
+	reply_markup=InlineKeyboardMarkup(
         [[
           InlineKeyboardButton("MAIN CHANNEL🎧", url="https://t.me/NAZRIYASUPPORT")
         ]]))
