8e8abdd894c9104757d703711bbfa1852a0418f5
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-26 16:01:04 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -9,14 +9,14 @@ from pyrogram.types import InlineKeyboardMarkup
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid=message.chat.id
-	await bot.send_text(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
 	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
 	chatid= message.chat.id
 	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
 	
-@Client.on_message(filters.command(["start"]) & filters.private, group=1)
+@Client.on_message(filters.command(["welcome"]) & filters.private, group=1)
 async def welcome(bot, update):
-
-  await bot.send_text(text=f"""heyiamhvdbdvdv""",
+      chatid=message.chat.id
+      await bot.send_message(text=f"""heyiamhvdbdvdv""",chat_id=chatid)
