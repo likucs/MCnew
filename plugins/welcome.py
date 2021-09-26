1ab6b68ffc96543454be4265d822a35504c60d07
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-26 13:35:36 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -8,17 +8,9 @@ from pyrogram.types import InlineKeyboardMarkup
 
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
-  buttons = [[
-        InlineKeyboardButton('Home ⚡', callback_data='start'),
-        InlineKeyboardButton('About 🚩', callback_data='about')
-    ],[
-        InlineKeyboardButton('Close 🔐', callback_data='close')
-    ]]
-    
-    reply_markup = InlineKeyboardMarkup(buttons)
-
 	chatid=message.chat.id
 	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+       
 	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
