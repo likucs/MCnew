07b19ebb7353fb91c4d321c132766fc18d5c5b17
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-26 15:41:08 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -9,7 +9,10 @@ from pyrogram.types import InlineKeyboardMarkup
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid=message.chat.id
-	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	await bot.send_photo(
+        chatid=message.chat.id
+        photo="https://telegra.ph/file/83dd03f6cd75ffb341a9e.jpg",
+        text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
        
 	
 @Client.on_message(filters.left_chat_member)
