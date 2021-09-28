ddd649ce8a4b53a651ea64dbcfef3ee9f6d4c1d8
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-28 15:14:54 +0530

    Delete lissa directory

--- lissa/hhrvd.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-# Sticker ID 
-@Client.on_message(filters.command(["wasim"]))
-async def stickerid(bot, message):   
-  if message.reply_to_message.sticker:
-    await message.replay("heyibfbfbbfbfbbf",)
-  else:
-    await message.replay("jdhhdhfbfbdndnfb",)
