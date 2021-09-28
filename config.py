44e6d167d3de7a32c0ffd97253035c581da8d7f0
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-28 16:28:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
+    
+    WARN_DATA_ID = int(os.environ.get("WARN_DATA_ID", "0"))
+    WARN_SETTINGS_ID = int(os.environ.get("WARN_SETTINGS_ID", "0"))
