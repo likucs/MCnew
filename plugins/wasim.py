3416792e9c02e622a43483b23289501cc6823425
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-28 09:28:06 +0530

    Update wasim.py

--- plugins/wasim.py
+++ plugins/wasim.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["wasim"]))
 async def wasim(bot, message):   
-    if message.reply_to_message.message:
+    if message.reply_to_message.sticker:
      msg = await bot.reply_text("jdbdvdvvdvfvdvdvdv")
     else:
      await msg.edit_text("hrhdvvfbfbfbfbbfbb")
