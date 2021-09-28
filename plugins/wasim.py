619bff4cfcec3c1a569aa7291148c38270d2069c
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-28 09:14:20 +0530

    Update wasim.py

--- plugins/wasim.py
+++ plugins/wasim.py
@@ -5,3 +5,4 @@ async def stickerid(bot, message):
     if message.reply_to_message.message:
      msg = await bot.reply_text("jdbdvdvvdvfvdvdvdv")
     else:
+     await msg.edit_text("hrhdvvfbfbfbfbbfbb")
