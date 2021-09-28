5d48c91171ed67354536f66982a3fb7c729b30a0
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-28 09:13:20 +0530

    Update wasim.py

--- plugins/wasim.py
+++ plugins/wasim.py
@@ -3,4 +3,5 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.message:
-       await bot.reply_text("jdbdvdvvdvfvdvdvdv")
+     msg = await bot.reply_text("jdbdvdvvdvfvdvdvdv")
+    else:
