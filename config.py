1331b6112f9307b21abb519d655265b42613006b
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-29 16:26:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,3 @@ class Config(object):
     ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
 
     DB_URL = os.environ.get("DATABASE_1", "")
- 
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
