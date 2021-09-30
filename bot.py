9840117209c9a7e750bcfc03b9d7c2427e90c5b4
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-30 12:10:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,6 +38,23 @@ PHOTOS = [
     "https://telegra.ph/file/fe47bf785fc127335ac1f.jpg",
 ]
 
+async def send_msg(user_id, message):
+	try:
+		await message.copy(chat_id=user_id)
+		return 200, None
+	except FloodWait as e:
+		await asyncio.sleep(e.x)
+		return send_msg(user_id, message)
+	except InputUserDeactivated:
+		return 400, f"{user_id} : deactivated\n"
+	except UserIsBlocked:
+		return 400, f"{user_id} : user is blocked\n"
+	except PeerIdInvalid:
+		return 400, f"{user_id} : user id invalid\n"
+	except Exception as e:
+		return 500, f"{user_id} : {traceback.format_exc()}\n"
+
+
 BOT_OWNER = int(os.environ["BOT_OWNER"])
 
 DATABASE_URL = os.environ["DATABASE_URL"]
