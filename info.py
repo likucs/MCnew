2216587dc6cbc69ec191991fcab9441cf73f5e70
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 08:19:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,8 +14,11 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 # Admins, Channels & Users
 
 ADMINS = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # MongoDB information
