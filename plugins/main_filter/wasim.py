6fcddbd44facd9e2298c03668318cad5cf53123c
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 12:59:18 +0530

    Update wasim.py

--- plugins/main_filter/wasim.py
+++ plugins/main_filter/wasim.py
@@ -1,3 +1,25 @@
+import os
+import re
+import io
+import pyrogram
+from pyrogram import filters, Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from info import ADMINS
+
+from wasim_faris.filter_db import add_filter
+from wasim_faris.filter_db import find_filter
+from wasim_faris.filter_db import get_filters
+from wasim_faris.filter_db import delete_filter
+from wasim_faris.filter_db import count_filters
+
+from wasim_faris.connect_db import add_connection
+from wasim_faris.connect_db import all_connections
+from wasim_faris.connect_db import if_active
+from wasim_faris.connect_db import delete_connection
+from wasim_faris.connect_db import active_connection
+from plugins.main_filter.Helpers import parser,split_quotes
+
+
 @Client.on_message(filters.private & filters.command(["connections"]))
 async def connections(client,message):
     userid = message.from_user.id
