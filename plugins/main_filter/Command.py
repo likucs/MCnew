d3b2b654de675de5976e9e2b8222316cab21ec15
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 13:01:23 +0530

    Update Command.py

--- plugins/main_filter/Command.py
+++ plugins/main_filter/Command.py
@@ -402,42 +402,6 @@ async def deleteconnection(client,message):
             await message.reply_text("This chat isn't connected to me!\nDo /connect to connect.", quote=True)
 
 
-@Client.on_message(filters.private & filters.command(["connections"]))
-async def connections(client,message):
-    userid = message.from_user.id
-    groupids = await all_connections(str(userid))
-    if groupids is None:
-        await message.reply_text(
-            "There are no active connections!! Connect to some groups first.",
-            quote=True
-        )
-        return
-    buttons = []
-    for groupid in groupids:
-        try:
-            ttl = await client.get_chat(int(groupid))
-            title = ttl.title
-            active = await if_active(str(userid), str(groupid))
-            if active:
-                act = " - ACTIVE"
-            else:
-                act = ""
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{title}:{act}"
-                    )
-                ]
-            )
-        except:
-            pass
-    if buttons:
-        await message.reply_text(
-            "Your connected group details ;\n\n",
-            reply_markup=InlineKeyboardMarkup(buttons),
-            quote=True
-        )
-
 @Client.on_message(filters.group & filters.text)
 async def give_filter(client,message):
     group_id = message.chat.id
