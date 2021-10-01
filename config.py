5a51b42bd4f868c6d53150897a6e4e819ad7c160
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 08:15:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,13 @@ class Config(object):
     ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
 
     FILTER_DB_URI = "mongodb+srv://wasim:wasim@cluster0.wc1o6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ 
+    ADMINS = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+
+
+    auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+  
+    AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 
     
