defafd2a21c424b0447354a40b1641d276c20bb9
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 13:56:14 +0530

    Update Farisx.py

--- Farisx.py
+++ Farisx.py
@@ -16,7 +16,6 @@ from config import Config
 LOGGER = logging.getLogger(__name__)
 APP_ID = Config.APP_ID
 API_HASH = Config.API_HASH
-MAX_MESSAGE_LENGTH = Config.MAX_MESSAGE_LENGTH
 TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
 # create download directory, if not exist
 if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
