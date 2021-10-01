c386a6847eea8b9f2126f8741b2d5ab7aa9224c8
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 08:00:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,7 +5,6 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
