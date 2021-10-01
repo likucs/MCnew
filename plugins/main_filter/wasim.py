eabaaeda4a6a6657428b867a1a0f941fc9588a5b
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 12:33:11 +0530

    Create wasim.py

---
+++ plugins/main_filter/wasim.py
@@ -0,0 +1,36 @@
+@Client.on_message(filters.private & filters.command(["connections"]))
+async def connections(client,message):
+    userid = message.from_user.id
+
+    groupids = await all_connections(str(userid))
+    if groupids is None:
+        await message.reply_text(
+            "There are no active connections!! Connect to some groups first.",
+            quote=True
+        )
+        return
+    buttons = []
+    for groupid in groupids:
+        try:
+            ttl = await client.get_chat(int(groupid))
+            title = ttl.title
+            active = await if_active(str(userid), str(groupid))
+            if active:
+                act = " - ACTIVE"
+            else:
+                act = ""
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{title}:{act}"
+                    )
+                ]
+            )
+        except:
+            pass
+    if buttons:
+        await message.reply_text(
+            "Your connected group details ;\n\n",
+            reply_markup=InlineKeyboardMarkup(buttons),
+            quote=True
+        )
