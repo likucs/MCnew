14eaa2febf5e5214e9f963ca71d17466aef566bc
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-01 10:54:56 +0530

    Update Command.py

--- plugins/main_filter/Command.py
+++ plugins/main_filter/Command.py
@@ -1,6 +1,7 @@
 import os
 import re
 import io
+import pyrogram
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import ADMINS
