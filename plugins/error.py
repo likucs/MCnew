05829e5c4f52002fd3c4f7239865c951998b4af6
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-02 17:32:37 +0530

    Create error.py

---
+++ plugins/error.py
@@ -0,0 +1,44 @@
+import os
+import pymongo
+
+if bool(os.environ.get("WEBHOOK", False)):
+from info import FILTER_DB_URI
+else:
+    from config import Config
+ 
+myclient = pymongo.MongoClient(FILTER_DB_URI)
+mydb = myclient["Cluster0"]
+mycol = mydb['USERS']
+
+
+
+
+async def add_user(id, username, name, dcid):
+    data = {
+        '_id': id,
+        'username' : username,
+        'name' : name,
+        'dc_id' : dcid
+    }
+    try:
+        mycol.update_one({'_id': id},  {"$set": data}, upsert=True)
+    except:
+        pass
+
+
+async def all_users():
+    count = mycol.count()
+    return count
+
+
+async def find_user(id):
+    query = mycol.find( {"_id":id})
+
+    try:
+        for file in query:
+            name = file['name']
+            username = file['username']
+            dc_id = file['dc_id']
+        return name, username, dc_id
+    except:
+        return None, None, None
