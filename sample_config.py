b57f62f6de27fcf1a3d8d944068fcce4f26c49ab
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-02 22:06:12 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,3 +5,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
 
 from typing import List
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
