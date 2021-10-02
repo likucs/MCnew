26699a202b877b1020729d0e92239da67cc316be
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-02 21:30:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,7 +9,8 @@ import re
 from pyrogram import Client as bot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
-
+import re
+from typing import List
 
 id_pattern = re.compile(r'^.\d+$')
 
@@ -48,6 +49,17 @@ FILTER_DB_URI = "mongodb+srv://wasim:wasim@cluster0.wc1o6.mongodb.net/myFirstDat
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 
 SUDO_USERS = "1287385877"
+
+WASIM_FARISX = "def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'"
 
 
 USE_AS_BOT = True
