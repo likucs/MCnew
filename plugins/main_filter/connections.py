0bf0a1796730d05cafe1099a76d93e8ba5457000
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-02 13:13:17 +0530

    Update connections.py

--- plugins/main_filter/connections.py
+++ plugins/main_filter/connections.py
@@ -35,7 +35,7 @@ async def connections(client,message):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{act}"
+                        text=f"{title}{act}", callback_data=f"wasimh:{groupid}:{act}"
                     )
                 ]
             )
