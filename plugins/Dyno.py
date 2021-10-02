ebdefe420d7d3bd91200ac5a3d1cb2ce028ad0e3
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-02 17:03:17 +0530

    Update Dyno.py

--- plugins/Dyno.py
+++ plugins/Dyno.py
@@ -13,7 +13,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 if bool(os.environ.get("WEBHOOK", False)):
 
-from plugins.Helpers import humanbytes
+from Wasimfarisx import humanbytes
 from wasim_faris.filter_db import filter_stats
 from wasim_faris.users_mdb import add_user, find_user, all_users
 
