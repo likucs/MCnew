cdeec8f8683af9514a1eb1c252ec77ea4f2d6d42
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-02 19:15:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,16 +13,6 @@ from pyrogram.types import InlineKeyboardButton
 from typing import List
 from info import AUTH_GROUPS
 
-def humanbytes(size):
-    if not size:
-        return ""
-    power = 2**10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 id_pattern = re.compile(r'^.\d+$')
 
