7db6db5116f86c4c8ad69829001c68317e410f12
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-02 15:38:25 +0530

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -1,1 +1,22 @@
-Hd
+
+import os
+import math
+import json
+import time
+import shutil
+import heroku3
+import requests
+
+from pyrogram import filters
+from pyrogram import Client as trojanz
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+from script import Script
+from plugins.helpers import humanbytes
+from database.filters_mdb import filter_stats
+from database.users_mdb import add_user, find_user, all_users
