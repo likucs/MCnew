7a6ddc40ac8827a73b5317479817c73673ad1436
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-03 16:06:54 +0530

    Create json.py

---
+++ plugins/json.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from io import BytesIO
+from config import *
+
+BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+        ]]
+    )
+
+@Client.on_message(filters.command(["json", "response"]), group=1)
+async def response_json(bot, update: Message):
+    json = update.reply_to_message
+    with BytesIO(str.encode(str(json))) as json_file:
+        json_file.name = "json.text"
+        await json.reply_document(
+            document=json_file,
+            reply_markup=BUTTONS,
+            quote=True
+        )
+        try:
+            os.remove(json_file)
+        except:
+            pass
