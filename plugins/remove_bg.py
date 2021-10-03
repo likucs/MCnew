3b9e5590e9b2cfea16dccbdfa127436759fc812b
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-03 12:21:44 +0530

    Update remove_bg.py

--- plugins/remove_bg.py
+++ plugins/remove_bg.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from pyrogram import Client, filters
+from pyrogram import Client as wasim
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
@@ -14,7 +14,7 @@ ERROR_BUTTONS = InlineKeyboardMarkup(
         ]]
     )
 
-@FayasNoushad.on_message(filters.private & (filters.photo | filters.video | filters.document))
+@wasim.on_message(filters.private & (filters.photo | filters.video | filters.document))
 async def remove_background(bot, update):
     if not REMOVEBG_API:
         await update.reply_text(
