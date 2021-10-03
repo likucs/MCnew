1a234e557d4ca27922b146cff02924260fb03fc4
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-03 11:28:17 +0530

    Create image-inline.py

---
+++ plugins/image-inline.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from requests.utils import requote_uri
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+API = "https://api.abirhasan.wtf/google?query="
+
+@Client.on_inline_query()
+async def inline(bot, update):
+    results = google(update.query)
+    answers = []
+    for result in results:
+        answers.append(
+            InlineQueryResultArticle(
+                title=result["title"],
+                description=result["description"],
+                input_message_content=InputTextMessageContent(
+                    message_text=result["text"],
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton(text="Link", url=result["link"])],
+                        JOIN_BUTTON
+                    ]
+                )
+            )
+        )
+    await update.answer(answers)
+
+
+def google(query):
+    r = requests.get(API + requote_uri(query))
+    informations = r.json()["results"][:50]
+    results = []
+    for info in informations:
+        text = f"**Title:** `{info['title']}`"
+        text += f"\n**Description:** `{info['description']}`"
+        text += f"\n\nMade by @FayasNoushad"
+        results.append(
+            {
+                "title": info['title'],
+                "description": info['description'],
+                "text": text,
+                "link": info['link']
+            }
+        )
+    return results
