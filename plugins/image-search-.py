8670075c7dc76bf7b9116ae67607c894d049553c
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-03 16:53:19 +0530

    Create image-search-.py

---
+++ plugins/image-search-.py
@@ -0,0 +1,22 @@
+import os
+import requests
+from requests.utils import requote_uri
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+API = "https://apibu.herokuapp.com/api/y-images?query="
+
+@Client.on_inline_query()
+async def search(bot, update):
+    results = requests.get(API + requote_uri(update.query)).json()["result"][:50]
+    answers = []
+    for result in results:
+        answers.append(
+            InlineQueryResultPhoto(
+                title=update.query,
+                description=result,
+                caption="Made by @FayasNoushad",
+                photo_url=result
+            )
+        )
+    await update.answer(answers)
