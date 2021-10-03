909a81dd743cc6cd3c4455a858c9f0be34bd085e
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-03 16:30:01 +0530

    Update corona.py

--- plugins/covid-19/corona.py
+++ plugins/covid-19/corona.py
@@ -9,7 +9,7 @@ API = "https://api.sumanjay.cf/covid/?country="
 
 BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')]])
 
-@Client.on_message(filters.private & filters.text)
+@Client.on_message(filters.private & filters.command(["covid"]))
 async def reply_info(bot, update):
     reply_markup = BUTTONS
     await update.reply_text(
