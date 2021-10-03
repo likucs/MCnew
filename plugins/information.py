fc5cedd70ed8983f1eca4a628f158c19dfe1b4a9
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-03 15:44:29 +0530

    Update information.py

--- plugins/information.py
+++ plugins/information.py
@@ -35,10 +35,10 @@ def user_info(user):
     text = "--**User Details:**--\n"
     text += f"\n**First Name:** `{user.first_name}`"
     text += f"\n**Last Name:** `{user.last_name},`" if user.last_name else ""
-    text += f"\n**User Id:** `{user.id}`"
-    text += f"\n**Username:** @{user.username}" if user.username else ""
-    text += f"\n**User Link:** {user.mention}" if user.username else ""
-    text += f"\n**DC ID:** `{user.dc_id}`" if user.dc_id else ""
+    text += f"\n**User Id:** `{user.id}`\n"
+    text += f"\n**Username:** @{user.username}" if user.username else \n""
+    text += f"\n**User Link:** {user.mention}" if user.username else\n ""
+    text += f"\n**DC ID:** `{user.dc_id}`" if user.dc_id else \n""
     text += f"\n**Is Deleted:** True" if user.is_deleted else ""
     text += f"\n**Is Bot:** True" if user.is_bot else ""
     text += f"\n**Is Verified:** True" if user.is_verified else ""
@@ -46,9 +46,8 @@ def user_info(user):
     text += f"\n**Is Scam:** True" if user.is_scam else ""
     text += f"\n**Is Fake:** True" if user.is_fake else ""
     text += f"\n**Is Support:** True" if user.is_support else ""
-    text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
-    text += f"\n**Status:** {user.status}" if user.status else ""
-    text += f"\n\nMade by @FayasNoushad"
+    text += f"\n**Language Code:** {user.language_code}" if user.language_code else \n""
+    text += f"\n**Status:** <code>{user.status}</code>" if user.status else ""
     return text
 
 
