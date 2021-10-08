0817a2f5e6c35a1a4943812864b5f215fd2e6d33
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-08 15:12:37 +0530

    Update setwelcome.py

--- plugins/setwelcome.py
+++ plugins/setwelcome.py
@@ -14,9 +14,17 @@ async def auto_welcome(bot: Client, msg: Message):
     group_name = msg.chat.title
     group_username = msg.chat.username
     welcome_text = f"👋Hey {mention}, Welcome To {group_name}\n\n Developed By @Mo_Tech_YT"
-    welcome_msg = os.environ.get("WELCOME_TEXT", welcome_text)
+    welcome_msg = os.environ.get(f"WELCOME_TEXT", welcome_text)
     print("Welcome Message Activate")
-    await msg.reply_text(text=welcome_msg)
+    await msg.reply_text(text=welcome_msg.format(
+        first = msg.from_user.first_name,
+        last = msg.from_user.last_name,
+        username = None if not msg.from_user.username else '@' + msg.from_user.username,
+        mention = msg.from_user.mention,
+        id = msg.from_user.id,
+        group_name = msg.chat.title,
+        group_username = None if not msg.chat.username else '@' + msg.chat.username
+   )
 
 @Client.on_message(filters.private & filters.command("admin"))
 async def admin(bot: Client, update):
