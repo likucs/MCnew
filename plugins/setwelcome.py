66089e9dd131aa7bd96f05a888ca743dbf26e367
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-08 15:16:38 +0530

    Update setwelcome.py

--- plugins/setwelcome.py
+++ plugins/setwelcome.py
@@ -25,38 +25,3 @@ async def auto_welcome(bot: Client, msg: Message):
         group_name = msg.chat.title,
         group_username = None if not msg.chat.username else '@' + msg.chat.username
    )
-
-
-@Client.on_message(filters.private & filters.command("admin"))
-async def admin(bot: Client, update):
-    # Heroku Support
-    user_admin = "Open Heroku => Application => Settings => Config Vars => Welcome_Text Edit"
-    user = "👋Hey {}, \n You are not the deploy of this bot"
-    run = "PR0FESS0R-99/Auto-Welcome-Bot" # https://github.com/PR0FESS0R-99/Auto-Welcome-Bot
-    api_key = os.environ.get("APP_NAME", "")
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split()) 
-    reply_markup=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton
-                    (
-                        "⚙️HEROKU SETTINGS⚙️", url=f"https://dashboard.heroku.com/apps/{api_key}/settings"
-                    )
-            ]
-        ]
-    )
-    deploy =InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton
-                    (
-                        "💫 DEPLOY NOW 💫", url=f"https://heroku.com/deploy?template=https://github.com/{run}/tree/main"
-                    )
-            ]
-        ]
-    )
-    if update.from_user.id not in OWNER_ID:
-        await update.reply_text(text=user.format(update.from_user.mention), reply_markup=deploy)
-        return
-    await update.reply_text(text=user_admin, reply_markup=reply_markup)
-   
