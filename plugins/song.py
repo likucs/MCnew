b63555f2c1bcf9869e9eee26b797c114073997ca
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-10 09:41:37 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -52,7 +52,7 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("<code>hey bruh iam Uploading your song pls wait"</code>)
+    m.edit("<code>hey bruh iam Uploading your song pls wait</code>")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
