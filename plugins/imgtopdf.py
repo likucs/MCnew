3c0cfc8f499e647adf5d650247251bb8803afac8
Aꜱᴡɪɴ <82755354+Aswin-TG@users.noreply.github.com>
2021-10-10 21:22:26 +0530

    Mm done 🙂

--- plugins/imgtopdf.py
+++ plugins/imgtopdf.py
@@ -10,34 +10,38 @@ LIST = {}
 @Client.on_message(filters.command("convertpdf"))
 async def pdf(client,message):
  
- if not isinstance(LIST.get(message.from_user.id), list):
-   LIST[message.from_user.id] = []
+    if not isinstance(LIST.get(message.from_user.id), list):
+      LIST[message.from_user.id] = []
 
-  
+    if not message.reply_to_message:
+        return await message.reply("`Reply to a photo to convert it to a pdf..`
+
+    if not message.reply_to_messagd.photo:
+        return await message.reply("`Reply to a photo to convert it to a pdf..`
  
- file_id = str(message.photo.file_id)
- ms = await message.reply_text("<code>Converting to PDF ..</code>")
- file = await client.download_media(file_id)
+    file_id = str(message.reply_to_message.photo.file_id)
+    ms = await message.reply_text("<code>Converting to PDF ..</code>")
+    file = await client.download_media(file_id)
  
- image = Image.open(file)
- img = image.convert('RGB')
- LIST[message.from_user.id].append(img)
- await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
+    image = Image.open(file)
+    img = image.convert('RGB')
+    LIST[message.from_user.id].append(img)
+    await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
  
 
 @Client.on_message(filters.command(['convert']))
 async def done(client,message):
- images = LIST.get(message.from_user.id)
+    images = LIST.get(message.from_user.id)
 
- if isinstance(images, list):
-  del LIST[message.from_user.id]
- if not images:
-  await message.reply_text( "No image !!")
-  return
+    if isinstance(images, list):
+     del LIST[message.from_user.id]
+    if not images:
+     await message.reply_text( "No image !!")
+     return
 
- path = f"{message.from_user.id}" + ".pdf"
- images[0].save(path, save_all = True, append_images = images[1:])
+    path = f"{message.from_user.id}" + ".pdf"
+    images[0].save(path, save_all = True, append_images = images[1:])
  
- await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here your pdf !!")
- os.remove(path)
+    await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here your pdf !!")
+    os.remove(path)
  
