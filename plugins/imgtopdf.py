2d301c2611e1a83c35ca7e5b9b54b7a7699546d2
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-10 18:47:21 +0530

    Update imgtopdf.py

--- plugins/imgtopdf.py
+++ plugins/imgtopdf.py
@@ -7,7 +7,7 @@ from pyrogram.types import (InlineKeyboardButton,  InlineKeyboardMarkup)
 
 LIST = {}
 
-@Client.on_message(filters.private & filters.photo)
+@Client.on_message(filters.command("convertpdf"))
 async def pdf(client,message):
  
  if not isinstance(LIST.get(message.from_user.id), list):
