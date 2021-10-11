9ee36bb5374a28993d0abde48f68c0650a019cf8
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-11 11:11:48 +0530

    Update sticker welcome.py

--- plugins/sticker welcome.py
+++ plugins/sticker welcome.py
@@ -3,7 +3,6 @@ from os import error
 import logging
 import pyrogram
 import time
-from decouple import config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import User, Message, Sticker, Document, ChatMember
