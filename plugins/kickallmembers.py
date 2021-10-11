08a06e8ad031fe392a533f9116816d782b9b0b79
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-11 22:39:43 +0530

    Update kickallmembers.py

--- plugins/kickallmembers.py
+++ plugins/kickallmembers.py
@@ -15,7 +15,7 @@ Then send /kick in the group and I will start my work.'''
 @Client.on_message(filters.group & filters.command("kickallmembers"))
 def main(_, msg: Message):
     chat = msg.chat
-    me = chat.get_member(Client.get_me().id)
+    me = chat.get_member(app.get_me().id)
     if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
         try:
             msg.reply(STARTED.format(chat.members_count))
