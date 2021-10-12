52b569a74ec419d49939ab1f39fecbd9b54b5c4e
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-12 15:00:38 +0530

    Create set_warn_limit.py

---
+++ plugins/warn/set_warn_limit.py
@@ -0,0 +1,13 @@
+import json
+from pyrogram import filters
+from pyrogram.types import (
+    Message
+)
+from pyrobot import (
+    COMMAND_HAND_LER,
+    WARN_SETTINGS_ID
+)
+
+from plugins.cust_p_filters import (
+    sudo_filter
+)
