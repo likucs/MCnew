922c25d1e258d71b555f67886f3612ce7a202422
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-12 17:26:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,11 +58,6 @@ USE_AS_BOT = True
 
 # Messages
 
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
