442450f23360f7de25892525df2466e985be7b51
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-12 09:44:17 +0530

    Update call_back.py

--- plugins/call_back.py
+++ plugins/call_back.py
@@ -347,20 +347,6 @@ async def callback_data(bot, update: CallbackQuery):
             parse_mode="html"
         )
 
-    elif query_data == "countryinfo": 
-        buttons = [[
-            InlineKeyboardButton('◀️ back', callback_data='home'),
-            InlineKeyboardButton('🏘 home', callback_data='start')
-        ]]
-        
-        reply_markup = InlineKeyboardMarkup(buttons)
-        
-        await update.message.edit_text(
-            COUNTRY_TEXT,
-            reply_markup=reply_markup,
-            parse_mode="html"
-        )
-
     elif query_data == "close":
         await update.message.delete()
 
