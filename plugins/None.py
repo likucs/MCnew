6d7808fec2a034de974d9bc463125330387f1c0d
Aꜱᴡɪɴ <82755354+Aswin-TG@users.noreply.github.com>
2021-10-13 10:49:53 +0530

    Update None.py

--- plugins/None.py
+++ plugins/None.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from wasim_faris.chat_col import present_in_chats, add_to_chats 
 
 @bot.on_message(filters.group, group=2)
-async def None(client, message):
+async def chats(client, message):
     if not await present_in_chats(message.chat.id):
         await add_to_chats(message.chat.id)
         print("New Group: {message.chat.title}")
