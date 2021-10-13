b67853f57e4bf5b39e913f65dd7f8ab1dba753ee
Aꜱᴡɪɴ <82755354+Aswin-TG@users.noreply.github.com>
2021-10-13 10:45:33 +0530

    Done

---
+++ plugins/None.py
@@ -0,0 +1,9 @@
+from pyrogram import Client as bot
+from pyrogram import filters
+from wasim_faris.chat_col import present_in_chats, add_to_chats 
+
+@bot.on_message(filters.group, group=2)
+async def None(client, message):
+    if not await present_in_chats(message.chat.id):
+        await add_to_chats(message.chat.id)
+        print("New Group: {message.chat.title}")
