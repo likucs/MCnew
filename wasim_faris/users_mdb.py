65e6c0d96520ac59aa3f1b9a54f53f16d5cb3959
Aꜱᴡɪɴ <82755354+Aswin-TG@users.noreply.github.com>
2021-10-13 10:35:22 +0530

    Update users_mdb.py

--- wasim_faris/users_mdb.py
+++ wasim_faris/users_mdb.py
@@ -5,6 +5,7 @@ from info import FILTER_DB_URI
 myclient = pymongo.MongoClient(FILTER_DB_URI)
 mydb = myclient["Cluster0"]
 mycol = mydb['USERS']
+chat_col = mydb['CHATS']
 
 async def add_user(id, username, name, dcid):
     data = {
@@ -40,3 +41,28 @@ async def find_user(id):
 async def get_all_users(self):
         all_users = self.col.find({})
         return all_users
+#---_---------------------_---------_------------------------------
+
+
+async def add_to_chats(chat_id: int):
+    return chat_col.insert_one({'_id': chat_id})
+    
+async def del_from_chats(chat_id: int):
+    return chat_col.delete_one({'_id': chat_id})
+    
+async def present_in_chats(chat_id : int):
+    found = chat_col.find_one({'_id': chat_id})
+    if found:
+        return True
+    else:
+        return False
+
+def chat_count():
+    return chat_col.estimated_document_count()
+
+async def get_chats():
+    chat_docs = chat_col.find()
+    chat_ids = []
+    for doc in chat_docs:
+        chat_col.append(doc['_id'])   
+    return chat_ids
