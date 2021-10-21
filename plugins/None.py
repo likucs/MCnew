54c009676fdd87850f4a36c3fa728fe88ea9aeab
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-21 09:37:30 +0530

    Delete None.py

--- plugins/None.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client as bot
-from pyrogram import filters
-
-@bot.on_message(filters.group, group=2)
-async def chats(client, message):
-    if not await present_in_chats(message.chat.id):
-        await add_to_chats(message.chat.id)
-        print("New Group: {message.chat.title}")
